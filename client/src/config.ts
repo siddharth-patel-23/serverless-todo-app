02f56077ddb4f211014f62c459ba3eb3bf6f0092
codeItSidz <sidz23999@gmail.com>
2020-06-10 19:06:16 +0530

    1st commit

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'jt4hw65gh8'
-export const apiEndpoint = `https://${apiId}.execute-api.us-west-1.amazonaws.com/dev`
+const apiId = 'vx8ebvr001'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-south-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'dev-rcrwdmsb.auth0.com',            // Auth0 domain
-  clientId: 'WLEug2J4NkBpWPE1m8P8WWxan7ZzTYC2',          // Auth0 client id
+  domain: 'sidz.auth0.com',            // Auth0 domain
+  clientId: 'zi6i12w7Spye9W9sq3tEa2akXM1d1m2n',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
